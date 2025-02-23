873e51db9605d7263fa78a51cf28381e0780e8b5
VDelarosa <vmdel@DESKTOP-QCM166L>
2025-02-22 18:00:30 -0600

    Add project files.

---
+++ WordSearch/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WordSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordsFilePath = "words.txt";
+            string[] minecraftBiomes = { "minecraftBiomes", "desert", "savanna", "jungle", "taiga", "meadow", "swamp", "badlands", "snowyTundra", "mushroomField", "bambooJungle", "dripstoneCave", "stonyPeaks", "cherryGrove", "forest", "beach" };
+            string[] avatarCharacters = { "avatarCharacters","aang", "katara", "sokka", "zuko", "iroh", "toph", "appa", "momo", "azula", "kingOzai", "suki", "jet", "kimgBumi", "roku", "princessYue" };
+            string[] undertaleCharacters = { "undertaleCharacters", "toriel", "papyrus", "sans", "asgore", "muffet", "napstablook", "undyne", "alphys", "mettaton", "flowey", "asriel", "frisk", "chara", "monsterKid", "temmie" };
+            string[] tarotCards = { "tarotCards", "theFool", "theMagician", "judgement", "kingOfWands", "threeOfCups", "tenOfSwords", "knightOfCoins", "highPriestess", "theLovers", "strength", "death", "temperance", "theTower", "theHangedMan", "justice" };
+            string[] breathOfTheWildIngredients = { "amoranth", "heartyRadish", "enduraCarrot", "hydromelon", "keeseWing", "mightyBananas", "rawMeat", "chuchuJelly", "roskSalt", "spicyPepper", "voltfruit", "wildberry", "ironshellCrab", "hylianRice", "hinoxGuts" };
+            string[] texasWildFlowers = {"texasWildFlowers", "bluebonnet", "brownEyedSusan", "mexicanBlanket", "texasThistle", "pricklyPoppy", "frogFruit", "greenMilkweed", "purpleConeflower", "yellowLotus", "henbit", "bluebells", "mistFlower", "passionFlower", "woodSorrel", "falseGarlic" };
+            string[] darkSoulsStats = { "darkSoulsStats", "vigor", "attunement", "endurance", "vitality", "stength", "dexterity", "intelligence", "faith", "luck", "bleedResistance", "poisonResistance", "frostResistance", "curseResistance", "magicDefense", "physicalDefense" };
+            string[] austinVenues = {"austinVenues", "mohawk", "chessClub", "theParish", "emos", "holeInTheWall", "empire", "thirteenthFloor", "ballroom", "kickbuttCoffee", "concourseProject", "farOutLounge", "pattonHaus", "hotelVegas", "radioEast", "theCoralSnake"};
+            string[] desserts = {"pudding", "cheesecake", "icecream", "donut", "cannoli", "baklava", "mochi", "jalebi", "cobbler", "pie", "flan", "bananaBread", "cupcake", "cinnamonRoll", "cookie"};
+            string[] appsOnMyPhone = { "discord", "googleClassroom", "pinterest", "depop", "planetFitness", "libby", "boyCat", "venmo", "tooGoodToGo", "lyft", "tidal", "gmail", "alliant", "busuu", "googleDrive" };
+
+        }
+    }
+}
