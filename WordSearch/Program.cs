ebc1be664d5739424d5470c93b266935feadd84a
VDelarosa <vmdel@DESKTOP-QCM166L>
2025-03-02 14:45:44 -0600

    Displaying only catergories from text file stored all text in array need to store categories only in array for user input comparison

--- WordSearch/Program.cs
+++ WordSearch/Program.cs
@@ -1,21 +1,41 @@
-﻿namespace WordSearch
+﻿    using System.IO;
+    namespace WordSearch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string wordsFilePath = "words.txt";
+            StreamWriter categoryWriter = new StreamWriter(wordsFilePath);
             string[] minecraftBiomes = { "minecraftBiomes", "desert", "savanna", "jungle", "taiga", "meadow", "swamp", "badlands", "snowyTundra", "mushroomField", "bambooJungle", "dripstoneCave", "stonyPeaks", "cherryGrove", "forest", "beach" };
             string[] avatarCharacters = { "avatarCharacters","aang", "katara", "sokka", "zuko", "iroh", "toph", "appa", "momo", "azula", "kingOzai", "suki", "jet", "kimgBumi", "roku", "princessYue" };
             string[] undertaleCharacters = { "undertaleCharacters", "toriel", "papyrus", "sans", "asgore", "muffet", "napstablook", "undyne", "alphys", "mettaton", "flowey", "asriel", "frisk", "chara", "monsterKid", "temmie" };
             string[] tarotCards = { "tarotCards", "theFool", "theMagician", "judgement", "kingOfWands", "threeOfCups", "tenOfSwords", "knightOfCoins", "highPriestess", "theLovers", "strength", "death", "temperance", "theTower", "theHangedMan", "justice" };
-            string[] breathOfTheWildIngredients = { "amoranth", "heartyRadish", "enduraCarrot", "hydromelon", "keeseWing", "mightyBananas", "rawMeat", "chuchuJelly", "roskSalt", "spicyPepper", "voltfruit", "wildberry", "ironshellCrab", "hylianRice", "hinoxGuts" };
+            string[] breathOfTheWildIngredients = { "breathOfTheWildIngredients", "amoranth", "heartyRadish", "enduraCarrot", "hydromelon", "keeseWing", "mightyBananas", "rawMeat", "chuchuJelly", "roskSalt", "spicyPepper", "voltfruit", "wildberry", "ironshellCrab", "hylianRice", "hinoxGuts" };
             string[] texasWildFlowers = {"texasWildFlowers", "bluebonnet", "brownEyedSusan", "mexicanBlanket", "texasThistle", "pricklyPoppy", "frogFruit", "greenMilkweed", "purpleConeflower", "yellowLotus", "henbit", "bluebells", "mistFlower", "passionFlower", "woodSorrel", "falseGarlic" };
             string[] darkSoulsStats = { "darkSoulsStats", "vigor", "attunement", "endurance", "vitality", "stength", "dexterity", "intelligence", "faith", "luck", "bleedResistance", "poisonResistance", "frostResistance", "curseResistance", "magicDefense", "physicalDefense" };
             string[] austinVenues = {"austinVenues", "mohawk", "chessClub", "theParish", "emos", "holeInTheWall", "empire", "thirteenthFloor", "ballroom", "kickbuttCoffee", "concourseProject", "farOutLounge", "pattonHaus", "hotelVegas", "radioEast", "theCoralSnake"};
-            string[] desserts = {"pudding", "cheesecake", "icecream", "donut", "cannoli", "baklava", "mochi", "jalebi", "cobbler", "pie", "flan", "bananaBread", "cupcake", "cinnamonRoll", "cookie"};
-            string[] appsOnMyPhone = { "discord", "googleClassroom", "pinterest", "depop", "planetFitness", "libby", "boyCat", "venmo", "tooGoodToGo", "lyft", "tidal", "gmail", "alliant", "busuu", "googleDrive" };
-
+            string[] desserts = { "desserts", "pudding", "cheesecake", "icecream", "donut", "cannoli", "baklava", "mochi", "jalebi", "cobbler", "pie", "flan", "bananaBread", "cupcake", "cinnamonRoll", "cookie"};
+            string[] appsOnMyPhone = {"appsOnMyPhone", "discord", "googleClassroom", "pinterest", "depop", "planetFitness", "libby", "boyCat", "venmo", "tooGoodToGo", "lyft", "tidal", "gmail", "alliant", "busuu", "googleDrive"};
+            IterateThrough(categoryWriter, minecraftBiomes);
+            IterateThrough(categoryWriter, avatarCharacters);
+            IterateThrough(categoryWriter, undertaleCharacters);
+            IterateThrough(categoryWriter, tarotCards);
+            IterateThrough(categoryWriter, breathOfTheWildIngredients);
+            IterateThrough(categoryWriter, texasWildFlowers);
+            IterateThrough(categoryWriter, darkSoulsStats);
+            IterateThrough(categoryWriter, austinVenues);
+            IterateThrough(categoryWriter, desserts);
+            IterateThrough(categoryWriter, appsOnMyPhone);
+            categoryWriter.Close();
+        }
+        private static void IterateThrough(StreamWriter wordWriter, string[] categoryName)
+        {
+            for (int i = 0; i < categoryName.Length; i++)
+            {
+                wordWriter.WriteLine(categoryName[i]);
+                wordWriter.Flush();
+            }
         }
     }
 }
--- puzzle/Program.cs
+++ puzzle/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace puzzle
+﻿using System.IO;
+namespace puzzle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StreamReader categoryReader = new StreamReader("words.txt");
+            string? allText = categoryReader.ReadLine();
+            string[] allTextAndCategories = new string[160];
+            int indexCounter = 0;
+            while(allText != null)
+            {
+                allTextAndCategories[indexCounter] = allText;
+                allText = categoryReader.ReadLine();
+                indexCounter++;
+            }
+            categoryReader.Close();
+            //Console.WriteLine(allTextAndCategories[0]);
+            //Console.WriteLine(allTextAndCategories[159]);
+            string[] categories = new string[14];
+            indexCounter = 0;
+            for(indexCounter = 0; indexCounter < allTextAndCategories.Length; indexCounter++)
+            {
+                if (indexCounter % 16 == 0)
+                {
+                    Console.WriteLine(allTextAndCategories[indexCounter]);
+                }
+            }
+           
+            
+            string[] fillerLetters = { "a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[,] wordSearchBoard = new string[20, 20];
+            bool isChoosing = true;
+            bool isPlaying = true;
+            Random randomize = new Random();
+
+            while (isPlaying)
+            {
+
+                while (isChoosing)
+                {
+                    Console.WriteLine("Please enter one of the above categories to begin your wordsearch.");
+                    string? chosenCategory = Console.ReadLine();
+                    if (chosenCategory == null)
+                    {
+                        Console.WriteLine("Invalid input, try again.");
+                        isChoosing = false;
+                    }
+                    //else if (chosenCategory ==)
+                    {
+
+                    }
+                }
+            }
+           
         }
     }
 }
